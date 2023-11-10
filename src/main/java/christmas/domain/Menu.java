3cb50080902523c5942ad73c4b782af3669a423f
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-10 11:45:13 +0900

    feat: Category Enum과 Menu Class 작성

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,11 @@
+package christmas.domain;
+
+public class Menu {
+    private final String viewName;
+    private final int price;
+
+    public Menu(String viewName, int price) {
+        this.viewName = viewName;
+        this.price = price;
+    }
+}
