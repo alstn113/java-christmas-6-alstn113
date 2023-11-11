f9b2afd9b1e99c38836cb9f8ab5c321746e14517
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-11 22:46:28 +0900

    feat: OrderItem 수량, 존재 예외 처리 및 테스트

--- src/main/java/christmas/domain/OrderItem.java
+++ src/main/java/christmas/domain/OrderItem.java
@@ -1,6 +1,27 @@
 package christmas.domain;
 
 public record OrderItem(String menuName, int quantity) {
+
+    public OrderItem {
+        validate(menuName, quantity);
+    }
+
+    private void validate(String menuName, int quantity) {
+        validateQuantity(quantity);
+        validateIsExistMenu(menuName);
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("수량은 1개 이상이어야 합니다.");
+        }
+    }
+
+    private void validateIsExistMenu(String menuName) {
+        Category.findMenuByName(menuName);
+    }
+
+
     public int price() {
         return menu().price() * quantity;
     }
@@ -12,4 +33,6 @@ public record OrderItem(String menuName, int quantity) {
     public Menu menu() {
         return Category.findMenuByName(menuName);
     }
+
+
 }
--- src/test/java/christmas/domain/OrderItemTest.java
+++ src/test/java/christmas/domain/OrderItemTest.java
@@ -1,13 +1,14 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("OrderItem 클래스 테스트")
 class OrderItemTest {
-
     @DisplayName("price 메서드는 주문 항목의 가격을 반환한다.")
     @Test
     void testPrice() {
@@ -28,5 +29,23 @@ class OrderItemTest {
         OrderItem orderItem = new OrderItem("레드와인", 1);
         assertThat(orderItem.menu()).extracting(Menu::name, Menu::price)
                 .containsExactly("레드와인", 60000);
+    }
+
+    @DisplayName("OrderItem 예외 테스트")
+    @Nested
+    class OrderItemExceptionTest {
+        @DisplayName("주문 수량이 1 미만일 경우 예외가 발생한다.")
+        @Test
+        void testQuantity() {
+            assertThatThrownBy(() -> new OrderItem("양송이수프", 0))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("존재하지 않는 메뉴일 경우 예외가 발생한다.")
+        @Test
+        void testIsExistMenu() {
+            assertThatThrownBy(() -> new OrderItem("존재하지 않는 메뉴", 1))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
