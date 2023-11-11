8c2034cdad4b6d202f4adb19becb19cb949afaac
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-11 10:58:31 +0900

    feat: OutputView base interface

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,21 @@
+package christmas.view;
+
+public interface OutputView {
+    void displayWelcomeMessage();
+
+    void displayEventPreviewMessage();
+
+    void displayOrderedMenu();
+
+    void displayTotalPriceBeforeDiscount();
+
+    void displayGiftMenu();
+
+    void displayBenefitDetails();
+
+    void displayTotalBenefitAmount();
+
+    void displayTotalPriceAfterDiscount();
+
+    void displayDecemberEventBadge();
+}
