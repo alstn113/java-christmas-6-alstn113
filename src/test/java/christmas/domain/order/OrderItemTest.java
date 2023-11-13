61065b343b3f9d5aa8de5d4af1153aa44bdf0ae4
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-14 02:07:01 +0900

    refactor: OrderItemTest 개선

--- src/test/java/christmas/domain/order/OrderItemTest.java
+++ src/test/java/christmas/domain/order/OrderItemTest.java
@@ -3,48 +3,46 @@ package christmas.domain.order;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("OrderItem 클래스 테스트")
 class OrderItemTest {
-    @DisplayName("price 메서드는 주문 항목의 가격을 반환한다.")
-    @Test
-    void testPrice() {
-        OrderItem orderItem = new OrderItem("양송이수프", 2);
-        assertThat(orderItem.getTotalPrice()).isEqualTo(12000);
+    @ParameterizedTest
+    @CsvSource(value = {"양송이수프, 2, 12000", "티본스테이크, 3, 165000", "레드와인, 1, 60000"})
+    void getTotalPrice메서드는_주문_항목의_가격을_반환한다(String menuName, int quantity, int totalPrice) {
+        OrderItem orderItem = new OrderItem(menuName, quantity);
+        assertThat(orderItem.getTotalPrice()).isEqualTo(totalPrice);
     }
 
-    @DisplayName("category 메서드는 주문 항목의 카테고리를 반환한다.")
     @Test
-    void testCategory() {
+    void isSameCategory메서드는_주문_항목의_카테고리를_반환한다() {
         OrderItem orderItem = new OrderItem("타파스", 1);
         assertThat(orderItem.isSameCategory(Category.APPETIZER)).isTrue();
     }
 
-    @DisplayName("menu 메서드는 주문 항목의 메뉴를 반환한다.")
     @Test
-    void testMenu() {
+    void getMenu메서드는_주문_항목의_메뉴를_반환한다() {
         OrderItem orderItem = new OrderItem("레드와인", 1);
         assertThat(orderItem.getMenu()).extracting(Menu::getViewName, Menu::getPrice)
                 .containsExactly("레드와인", 60000);
     }
 
-    @DisplayName("OrderItem 예외 테스트")
     @Nested
-    class OrderItemExceptionTest {
-        @DisplayName("주문 수량이 1 미만일 경우 예외가 발생한다.")
-        @Test
-        void testQuantity() {
-            assertThatThrownBy(() -> new OrderItem("양송이수프", 0))
+    class 예외_테스트 {
+        @ParameterizedTest
+        @ValueSource(ints = {-100, -1, 0})
+        void 주문_수량이_1보다_작을_경우_예외가_발생한다(int quantity) {
+            assertThatThrownBy(() -> new OrderItem("양송이수프", quantity))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
-        @DisplayName("존재하지 않는 메뉴일 경우 예외가 발생한다.")
-        @Test
-        void testIsExistMenu() {
-            assertThatThrownBy(() -> new OrderItem("존재하지 않는 메뉴", 1))
+        @ParameterizedTest
+        @ValueSource(strings = {"존재하지 않는 메뉴", "unknown"})
+        void 존재하지_않는_메뉴일_경우_예외가_발생한다(String menuName) {
+            assertThatThrownBy(() -> new OrderItem(menuName, 1))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
