d3e15347035c2c420d003a99c47ce9d4fc4ca323
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-14 01:21:59 +0900

    refactor: VisitDateTest 리팩토링

--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -4,35 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("VisitDate 클래스 테스트")
 class VisitDateTest {
-    @DisplayName("생성자 테스트")
-    @Test
-    void constructor() {
-        int day = 1;
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 15, 30, 31})
+    void day를_입력하면_2023년_12월_day일_LocalDate를_반환한다(int day) {
         assertThat(new VisitDate(day).getDate())
                 .isEqualTo(LocalDate.of(2023, 12, day));
     }
 
-    @Nested
-    @DisplayName("예외 테스트")
-    class ExceptionTest {
-        @DisplayName("1보다 작은 날짜 입력 시 예외 발생")
-        @Test
-        void lessThanOne() {
-            assertThatThrownBy(() -> new VisitDate(0))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
 
-        @DisplayName("31보다 큰 날짜 입력 시 예외 발생")
-        @Test
-        void moreThanThirtyOne() {
-            assertThatThrownBy(() -> new VisitDate(32))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -1, 0, 32, 100})
+    void 날짜는_1부터_31까지만_입력할_수_있다(int day) {
+        assertThatThrownBy(() -> new VisitDate(day))
+                .isInstanceOf(IllegalArgumentException.class);
     }
+
 }
