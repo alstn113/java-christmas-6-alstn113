88ce18bdaa4606fd34e2d9afc559ee7ef2858497
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-14 14:02:06 +0900

    refactor: ChristmasDdayDiscountStrategyTest 개선

--- src/test/java/christmas/domain/event/ChristmasDdayDiscountStrategyTest.java
+++ src/test/java/christmas/domain/event/ChristmasDdayDiscountStrategyTest.java
@@ -7,53 +7,50 @@ import christmas.domain.order.OrderItem;
 import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-@DisplayName("ChristmasDdayDiscountStrategy 클래스")
 class ChristmasDdayDiscountStrategyTest {
-
     private static ChristmasDdayDiscountStrategy strategy;
     private static Order order;
 
     @BeforeAll
     static void setUp() {
-        strategy = new ChristmasDdayDiscountStrategy();
         List<OrderItem> orderItems = List.of(
                 new OrderItem("티본스테이크", 2),
                 new OrderItem("초코케이크", 5),
                 new OrderItem("아이스크림", 3),
                 new OrderItem("제로콜라", 3)
         );
+
+        strategy = new ChristmasDdayDiscountStrategy();
         order = new Order(orderItems);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"1, 1000", "2, 1100", "3, 1200", "15, 2400", "23, 3200", "24, 3300", "25, 3400"})
+    void applyEventIfApplicable메서드는_1000원으로_시작하여_크리스마스가_다가올수록_날마다_할인_금액이_100원씩_증가한다(int day, int discountAmount) {
+        LocalDate currentDate = LocalDate.of(2023, 12, day);
 
-    @Test
-    @DisplayName("12월 1일 ~ 12월 25일이 아닌 경우 0을 반환한다.")
-    void Test1() {
-        LocalDate date1 = LocalDate.of(2023, 11, 30);
-        LocalDate date2 = LocalDate.of(2023, 12, 26);
-        LocalDate date3 = LocalDate.of(2024, 1, 1);
+        final EventResult result = strategy.applyEventIfApplicable(currentDate, order);
 
-        assertThat(strategy.applyEventIfApplicable(date1, order).getDiscountAmount()).isZero();
-        assertThat(strategy.applyEventIfApplicable(date2, order).getDiscountAmount()).isZero();
-        assertThat(strategy.applyEventIfApplicable(date3, order).getDiscountAmount()).isZero();
+        assertThat(result.getDiscountAmount()).isEqualTo(discountAmount);
     }
 
-    @Test
-    @DisplayName("1000원을 시작하여 1일이 지날 때마다 100원씩 할인한다.")
-    void applyEventIfApplicableTest2() {
-        LocalDate date1 = LocalDate.of(2023, 12, 1);
-        LocalDate date2 = LocalDate.of(2023, 12, 2);
-        LocalDate date3 = LocalDate.of(2023, 12, 3);
-        LocalDate date15 = LocalDate.of(2023, 12, 15);
-        LocalDate date25 = LocalDate.of(2023, 12, 25);
+    @Nested
+    class isApplicable메서드_테스트 {
+        @Test
+        void 현재_날짜가_12월_1일_이상_12월_25일_이하가_아닌_경우_false를_리턴한다() {
+            LocalDate beforeEventDate = LocalDate.of(2023, 11, 30);
+            LocalDate afterEventDate = LocalDate.of(2023, 12, 26);
 
-        assertThat(strategy.applyEventIfApplicable(date1, order).getDiscountAmount()).isEqualTo(1000);
-        assertThat(strategy.applyEventIfApplicable(date2, order).getDiscountAmount()).isEqualTo(1100);
-        assertThat(strategy.applyEventIfApplicable(date3, order).getDiscountAmount()).isEqualTo(1200);
-        assertThat(strategy.applyEventIfApplicable(date15, order).getDiscountAmount()).isEqualTo(2400);
-        assertThat(strategy.applyEventIfApplicable(date25, order).getDiscountAmount()).isEqualTo(3400);
+            final boolean beforeEventResult = strategy.isApplicable(beforeEventDate, order);
+            final boolean afterEventResult = strategy.isApplicable(afterEventDate, order);
+
+            assertThat(beforeEventResult).isFalse();
+            assertThat(afterEventResult).isFalse();
+        }
     }
 }
