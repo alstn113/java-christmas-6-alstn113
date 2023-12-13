7f7b3a5101c645a912849d2f617d18a544a6006e
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-13 15:38:44 +0900

    feat: 프로젝트 기본 구조 작성

---
+++ src/main/java/christmas/exception/ErrorMessage.java
@@ -0,0 +1,16 @@
+package christmas.exception;
+
+public enum ErrorMessage {
+    INPUT_NOT_A_NUMBER("입력값은 숫자여야 합니다."),
+    ;
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/christmas/view/util/InputUtil.java
@@ -0,0 +1,64 @@
+package christmas.view.util;
+
+import christmas.exception.ErrorMessage;
+import christmas.exception.InvalidInputException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class InputUtil {
+    private InputUtil() {
+    }
+
+    public static int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(ErrorMessage.INPUT_NOT_A_NUMBER);
+        }
+    }
+
+
+    public static List<String> parseToList(String input, String delimiter) {
+        if (input == null || input.isBlank()) {
+            return List.of();
+        }
+        String[] parts = input.split(delimiter, -1);
+        return Arrays.stream(parts).map(String::trim).toList();
+    }
+
+    public static <T> T retryOnException(Supplier<T> supplier, boolean lineBreak) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                if (lineBreak) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static void retryOnException(Runnable runnable, boolean lineBreak) {
+        while (true) {
+            try {
+                runnable.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                if (lineBreak) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static <T> T retryOnException(Supplier<T> supplier) {
+        return retryOnException(supplier, false);
+    }
+
+    public static void retryOnException(Runnable runnable) {
+        retryOnException(runnable, false);
+    }
+}
