d4a1174f485d1d0c64533b2a6c780b8cf315126b
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-14 14:17:15 +0900

    feat: 이벤트 뱃지를 작성한다.

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,25 @@
+package christmas.domain;
+
+import java.util.Arrays;
+
+public enum Badge {
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("별", 5_000),
+    NONE("없음", 0);
+
+    private final String name;
+    private final int price;
+
+    Badge(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Badge of(int conditionPrice) {
+        return Arrays.stream(values())
+                .filter(badge -> conditionPrice >= badge.price)
+                .findFirst()
+                .orElse(NONE);
+    }
+}
